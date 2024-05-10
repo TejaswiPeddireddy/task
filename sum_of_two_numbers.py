0ce8c296525a70bde68edffe5467ee6f6fd258e4
TejaswiPeddireddy <83960460+TejaswiPeddireddy@users.noreply.github.com>
2024-05-11 00:01:14 +0530

    Add files via upload

---
+++ sum_of_two_numbers.py
@@ -0,0 +1,9 @@
+
+# Define the function for sum of two numbers
+
+def sum_of_two_numbers(num1, num2):
+    return num1 + num2
+
+# Test the function with some numbers
+result = sum_of_two_numbers(3, 5)
+print(result)
